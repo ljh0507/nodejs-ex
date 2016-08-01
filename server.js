0de2f241259b7f6b0e9cd753501a0ed514e0ca20
ljh0507 <ljh8757@naver.com>
2016-08-01 14:16:29 +0900

    20160801 14:16 UPDATE

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '10.1.82.7',
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '172.30.0.159',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
